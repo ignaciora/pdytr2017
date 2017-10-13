aa0ee93154bc3084d04bb5b20b4895e680236496
Ignacio Rodriguez <nacho.rodriguez.a@gmail.com>
2017-10-13 18:31:31 -0400

    funcion copy

--- tp2/ej4-2/fs_client.c
+++ tp2/ej4-2/fs_client.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 #ifndef FILE_CHUNK_SIZE
 	#define FILE_CHUNK_SIZE 255
@@ -17,7 +18,7 @@ void fs_prog_1(char *host, char opt, char *source_file, char *dest_file)
 
 	clnt = clnt_create(host, FS_PROG, FS_VERSION, "udp");
 	if (clnt == NULL) {
-		clnt_pcreateerror (host);
+		clnt_pcreateerror(host);
 		exit(1);
 	}
 
@@ -48,33 +49,32 @@ void fs_prog_1(char *host, char opt, char *source_file, char *dest_file)
 /* Devuelve 0 si se logro extraer del server el archivo completo y 1 si se lo extrajo en forma parcial o si no se recupero nada. */
 int fs_read(char *source_file, char *dest_file, CLIENT *clnt)
 {
-	FILE *fd;
+	FILE *file;
 	buffer *file_part;
 	int reads_count = 0;
 
-	fd = fopen(dest_file, "wb");
+	file = fopen(dest_file, "wb");
+	if (file == NULL) {
+		return 1;
+	}
 
 	do {
 		file_part = read_file_1((filename)source_file, reads_count * FILE_CHUNK_SIZE, FILE_CHUNK_SIZE, clnt);
-
 		if (file_part == (buffer *) NULL) {
 			clnt_perror (clnt, "read: (ERROR) Fallo la llamada al server\n");
 			return 1;
 		}
 
 		if (file_part->buffer_len == -1) {
-			printf("read: (ERROR) El archivo %s no existe\n.", source_file);
 			return 1;
 		}
 
-		fwrite(file_part->buffer_val, file_part->buffer_len, 1, fd);
-
+		fwrite(file_part->buffer_val, file_part->buffer_len, 1, file);
 		reads_count++;
 	}
 	while (file_part->buffer_len == FILE_CHUNK_SIZE);
 
-	fclose(fd);
-
+	fclose(file);
 	return 0;
 }
 
@@ -82,45 +82,28 @@ int fs_read(char *source_file, char *dest_file, CLIENT *clnt)
 /* Devuelve 0 si se logro depositar en el server el archivo completo y 1 si se lo deposito en forma parcial o si no se guardo nada. */
 int fs_write(char *source_file, char *dest_file, CLIENT *clnt)
 {
-	/*
-	Abrir un archivo
-	Saber cuanto tengo que pasar en total
-	Hasta que haya completado el tamaño del archivo:
-		Levantar con lseek el cacho de archivo (CHUNK SIZE)
-		Mandar al servidor el cacho de archivo
-		Actualizo el tamaño de lo que llevo leyendo con lo que escribio el server
-	
-	Cierro el archivo
-	*/
-	printf("Funciono, luego existo");
+	FILE *file;
 	buffer *file_part;
-	file_part = malloc(sizeof(buffer));
-	file_part->buffer_val = malloc(FILE_CHUNK_SIZE * sizeof(buffer));
-
 	int *result;
 	int written_bytes = 0;
 	struct stat file_stat;
 
-	//Abrir un archivo
-	FILE *fd;
-	fd = fopen(source_file, "rb");
+	file = fopen(source_file, "rb");
+	if (file == NULL) {
+		return 1;
+	}
 	
-	//Saber cuanto tengo que pasar en total
-	if (fstat(fd->_fileno, &file_stat) == -1) {
+	if (fstat(file->_fileno, &file_stat) == -1) {
 		return 1;
 	}
 
-	printf("Logre abrir el archivo y cargar los stats");
+	file_part = malloc(sizeof(buffer));
+	file_part->buffer_val = malloc(FILE_CHUNK_SIZE * sizeof(buffer));
 
-	//Hasta que haya completado el tamaño del archivo:
 	do {
+		lseek(file->_fileno, written_bytes, SEEK_SET);
 
-		//Levantar con lseek el cacho de archivo (CHUNK SIZE)
-		lseek(fd->_fileno, written_bytes, SEEK_SET);
-		printf("hago el seek del archivo");
-		file_part->buffer_len = read(fd->_fileno, file_part->buffer_val, FILE_CHUNK_SIZE);
-
-		printf("Logre leer %i bytes del archivo", &file_part->buffer_len);
+		file_part->buffer_len = read(file->_fileno, file_part->buffer_val, FILE_CHUNK_SIZE);
 		if (file_part->buffer_len == -1) {
 			printf("read: (ERROR) El archivo %s no existe\n.", source_file);
 			return 1;
@@ -132,28 +115,53 @@ int fs_write(char *source_file, char *dest_file, CLIENT *clnt)
 			return 1;
 		}
 
-		printf("El server escribio: %i", &result);
 		written_bytes += *result;
-
 	}
 	while (file_stat.st_size > written_bytes);
 
-	//Cierro el archivo
-	fclose(fd);
-
+	fclose(file);
 	return 0;
 }
 
+
+/* TEST FUNCTION */
+void copy_gen(char *host, char *source_file, char *dest_file)
+{
+	CLIENT *clnt;
+	
+	clnt = clnt_create(host, FS_PROG, FS_VERSION, "udp");
+	if (clnt == NULL) {
+		clnt_pcreateerror(host);
+		exit(1);
+	}
+
+	if (fs_read(source_file, dest_file, clnt) != 0) {
+		printf("read: (ERROR) Fallo la lectura total o parcial del archivo que se encuentra en el servidor\n");
+	}
+
+	if (fs_write(dest_file, strcat(dest_file, "__copy"), clnt) != 0) {
+		printf("write: (ERROR) Fallo la escritura total o parcial de la copia en el servidor\n");
+	}
+
+	/* diff */
+
+	clnt_destroy(clnt);
+}
 
 
 int main (int argc, char *argv[])
 {
 	if (argc < 5) {
-		printf("usage: %s server_host -r|-w source_file dest_file\n", argv[0]);
+		printf("usage: %s server_host ([--copy]  |  -r|-w) source_file dest_file\n", argv[0]);
 		exit(1);
 	}
 
-	fs_prog_1(argv[1], argv[2][1], argv[3], argv[4]);
+	if (strcmp(argv[2], "--copy") == 0) {
+		copy_gen(argv[1], argv[3], argv[4]);
+	}
+	else {
+		fs_prog_1(argv[1], argv[2][1], argv[3], argv[4]);
+	}
 
 	exit(0);
 }
--- tp2/ej4-2/fs_server.c
+++ tp2/ej4-2/fs_server.c
@@ -5,24 +5,26 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 
 
 buffer *
 read_file_1_svc(filename filename, int offset, int bytes,  struct svc_req *rqstp)
 {
 	static buffer result;
+	FILE *file;
 
-	FILE *fd;
-	fd = fopen(filename, "rb");
+	file = fopen(filename, "rb");
+	if (file == NULL) {
+		return NULL;
+	}
 
-	lseek(fd->_fileno, offset, SEEK_SET);
+	lseek(file->_fileno, offset, SEEK_SET);
 	result.buffer_val = malloc(bytes * sizeof(char));
-	result.buffer_len = read(fd->_fileno, result.buffer_val, bytes);
+	result.buffer_len = read(file->_fileno, result.buffer_val, bytes);
 
-	printf("Leidos %d bytes del archivo %s", result.buffer_len, filename);
+	printf("read: %d bytes leidos del archivo %s\n", result.buffer_len, filename);
 	
-	fclose(fd);
+	fclose(file);
 	return &result;
 }
 
@@ -31,13 +33,17 @@ int *
 write_file_1_svc(filename filename, buffer buffer, int bytes,  struct svc_req *rqstp)
 {
 	static int result;
+	FILE *file;
 
-	FILE *fd;
-	fd = fopen(filename, "ab");
+	file = fopen(filename, "ab");
+	if (file == NULL) {
+		return 0;
+	}
 
-	result = fwrite(buffer.buffer_val, 1, buffer.buffer_len, fd);
-	printf("Se escribieron %d bytes\n", result);
+	result = fwrite(buffer.buffer_val, 1, buffer.buffer_len, file);
+	
+	printf("write: %d bytes escritos en el archivo %s\n", result, filename);
 
-	fclose(fd);
+	fclose(file);
 	return &result;
 }
