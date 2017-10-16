ef49b32df1d145402b77395c32696cedc9704f3d
Ignacio Rodriguez <nacho.rodriguez.a@gmail.com>
2017-10-16 00:53:43 -0400

    --help para el ej4

--- tp2/ej4-2/fs_client.c
+++ tp2/ej4-2/fs_client.c
@@ -163,19 +163,37 @@ void copy_gen(char *host, char *source_file, char *dest_file)
 }
 
 
+void show_help(char *program_name)
+{
+	printf("usage: %s [--help] server_host (-r|-w|--copy) source_file dest_file\n\n", program_name);
+	printf("--help			muestra esta ayuda\n");
+	printf("server_host		localhost o una IP\n");
+	printf("-r			read, traer un archivo del server\n");
+	printf("-w			write, enviar un archivo al server\n");
+	printf("--copy			trae un archivo del server y lo envia generando una copia remota\n");
+	printf("source_file		ruta del archivo local o nombre del archivo del server (origen)\n");
+	printf("dest_file		ruta del archivo local o nombre del archivo del server (destino)\n");
+}
+
+
 int main (int argc, char *argv[])
 {
+	if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+		show_help(argv[0]);
+		exit(0);
+	}
+
 	if (argc < 5) {
-		printf("usage: %s server_host (-r|-w|--copy) source_file dest_file\n", argv[0]);
+		printf("usage: %s [--help] server_host (-r|-w|--copy) source_file dest_file\n", argv[0]);
 		exit(1);
 	}
 
 	if (strcmp(argv[2], "--copy") == 0) {
 		copy_gen(argv[1], argv[3], argv[4]);
+		exit(0);
 	}
-	else {
-		fs_prog_1(argv[1], argv[2][1], argv[3], argv[4]);
-	}
+	
+	fs_prog_1(argv[1], argv[2][1], argv[3], argv[4]);
 
 	exit(0);
 }
